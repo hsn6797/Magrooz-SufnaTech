f7197b56fb810a5205bbee43150e4ee810c0364b
asad0ee <asadullah9555@gmail.com>
2020-08-16 14:40:15 +0500

    Added some file to git

---
+++ app/src/main/java/com/sufnatech/magrooz/Activities/VideoChatActivity.java
@@ -0,0 +1,71 @@
+package com.sufnatech.magrooz.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+import com.sufnatech.magrooz.R;
+
+import com.opentok.android.BaseVideoRenderer;
+import com.opentok.android.OpentokError;
+import com.opentok.android.Publisher;
+import com.opentok.android.PublisherKit;
+import com.opentok.android.Session;
+import com.opentok.android.Stream;
+import com.opentok.android.Subscriber;
+import com.opentok.android.SubscriberKit;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class VideoChatActivity extends AppCompatActivity  implements
+        Session.SessionListener,
+        PublisherKit.PublisherListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chat);
+    }
+
+    @Override
+    public void onStreamCreated(PublisherKit publisherKit, Stream stream) {
+
+    }
+
+    @Override
+    public void onStreamDestroyed(PublisherKit publisherKit, Stream stream) {
+
+    }
+
+    @Override
+    public void onError(PublisherKit publisherKit, OpentokError opentokError) {
+
+    }
+
+    @Override
+    public void onConnected(Session session) {
+
+    }
+
+    @Override
+    public void onDisconnected(Session session) {
+
+    }
+
+    @Override
+    public void onStreamReceived(Session session, Stream stream) {
+
+    }
+
+    @Override
+    public void onStreamDropped(Session session, Stream stream) {
+
+    }
+
+    @Override
+    public void onError(Session session, OpentokError opentokError) {
+
+    }
+}
