a29fc71e583631c354cb07d1b470b8a073d387b5
hsn6797 <hsn6797@gmail.com>
2020-08-18 22:09:51 +0500

    Basic functionality done (Revision--) - Successfully create session between two users

--- app/src/main/java/com/sufnatech/magrooz/Activities/StartLiveTalkActivity.java
+++ app/src/main/java/com/sufnatech/magrooz/Activities/StartLiveTalkActivity.java
@@ -1,17 +1,19 @@
 package com.sufnatech.magrooz.Activities;
 
-import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 import com.android.volley.Request;
@@ -20,12 +22,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -33,9 +29,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.opentok.android.Session;
 import com.sufnatech.magrooz.Helpers.Dialog.AlertDialog;
 import com.sufnatech.magrooz.Helpers.Dialog.AlertDialogSingleInterface;
 import com.sufnatech.magrooz.R;
@@ -59,7 +53,7 @@ public class StartLiveTalkActivity extends AppCompatActivity {
     Button startLiveTalk;
     FirebaseFirestore db;
 
-    String currentSessionID;
+    String currentSessionTableID="";
 
 
     String SESSION_ID = "";
@@ -100,7 +94,7 @@ public class StartLiveTalkActivity extends AppCompatActivity {
 
                         // Searching for Available Session on the bases of empty subscriberID field
                         if (task.isSuccessful() && task.getResult().size() > 0) {
-//                            Log.d(TAG, "Available Sessions\n");
+                            Log.d(TAG, "Available Sessions\n");
 
                             // 1- Select Random Session
                             List<DocumentSnapshot> sessionsList = task.getResult().getDocuments();
@@ -113,7 +107,8 @@ public class StartLiveTalkActivity extends AppCompatActivity {
                             // and move to next video screen
                             Map<String, Object> sessionMap = new HashMap<>();
                             sessionMap.put("publisherID", document.get("publisherID"));
-                            sessionMap.put("sessionKey", document.get("sessionKey"));
+                            sessionMap.put("sessionID", document.get("sessionID"));
+                            sessionMap.put("sessionToken", document.get("sessionToken"));
                             sessionMap.put("subscriberID", userID);
 
                             updateSessionSubsciberID(sessionMap,document.getId());
@@ -124,16 +119,19 @@ public class StartLiveTalkActivity extends AppCompatActivity {
                         } else{
 
                             // getting token ,Session,Apikey from server
-
                             fetchSessionforConnection();
-
-
+//                            if(!SESSION_ID.equals("") && !TOKEN.equals("")){
                             // 1- Create Session in database and move to next video screen
-                            Map<String, Object> sessionCreateMap = new HashMap<>();
-                            sessionCreateMap.put("publisherID", userID);
-                            sessionCreateMap.put("sessionKey", SESSION_ID);
-                            sessionCreateMap.put("subscriberID", "");
-                            createSession(sessionCreateMap);
+
+//                            }else{
+//                                AlertDialog.showSingleButtonAlertDialog(StartLiveTalkActivity.this,"Ok","Error",
+//                                        "Please try again later",new AlertDialogSingleInterface(){
+//                                    @Override
+//                                    public void doTaskOnClick() {
+//                                    }
+//                                });
+//                            }
+
                         }
                     }
                 });
@@ -145,8 +143,8 @@ public class StartLiveTalkActivity extends AppCompatActivity {
         super.onDestroy();
         // Delete the created user from database where id = userID
         deleteUser();
-        // Delete the created user from database where id = currentSessionID
-        //deleteSession();
+//         Delete the created user from database where id = currentSessionID
+        deleteSession();
 
         db = null;
     }
@@ -154,29 +152,42 @@ public class StartLiveTalkActivity extends AppCompatActivity {
 
     private void fetchSessionforConnection(){
 
+        // Loading Start
+        final Dialog dialog = AlertDialog.showLoadingDialog(StartLiveTalkActivity.this);
+        dialog.show();
+
         RequestQueue reqQueue = Volley.newRequestQueue(this);
         reqQueue.add(new JsonObjectRequest(Request.Method.GET,
                 "https://videoapptokbox.herokuapp.com" + "/session",
                 null, new Response.Listener<JSONObject>() {
-
             @Override
             public void onResponse(JSONObject response) {
                 try {
                     SESSION_ID = response.getString("sessionId");
                     TOKEN = response.getString("token");
 
+                    // Create Session in firebase
+                    Map<String, Object> sessionCreateMap = new HashMap<>();
+                    sessionCreateMap.put("publisherID", userID);
+                    sessionCreateMap.put("sessionID", SESSION_ID);
+                    sessionCreateMap.put("sessionToken", TOKEN);
+                    sessionCreateMap.put("subscriberID", "");
+                    createSession(sessionCreateMap);
+
+                    // Loading finished
+                    dialog.dismiss();
+
                     Log.i(Log_tag, "SESSION_ID: " + SESSION_ID);
                     Log.i(Log_tag, "TOKEN: " + TOKEN);
-
 
                 } catch (JSONException error) {
                     Log.e(Log_tag, "Web Service error: " + error.getMessage());
                 }
             }
         }, new Response.ErrorListener() {
-
             @Override
-            public void onErrorResponse(VolleyError error){
+            public void onErrorResponse(VolleyError error) {
+
             }
         }));
     }
@@ -184,59 +195,68 @@ public class StartLiveTalkActivity extends AppCompatActivity {
 
 
     private void createSession(final Map<String,Object> map){
-        // Loading Start
-        final Dialog dialog = AlertDialog.showLoadingDialog(StartLiveTalkActivity.this);
-        dialog.show();
+//        // Loading Start
+//        final Dialog dialog = AlertDialog.showLoadingDialog(StartLiveTalkActivity.this);
+//        dialog.show();
         db.collection("Sessions").add(map).
                 addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
-                        // Loading finished
-                        dialog.dismiss();
+//                        // Loading finished
+//                        dialog.dismiss();
 
                         Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
                         final String sessionID = documentReference.getId();
-                        currentSessionID = sessionID;
-                        // Go to StartLiveTalkActivity with sessionKey
+                        currentSessionTableID = sessionID;
+                        // Go to StartLiveTalkActivity with sessionID
                         Intent intent = new Intent(StartLiveTalkActivity.this,VideoChatActivity.class);
-                        intent.putExtra("sessionKey",map.get("sessionKey").toString());
+                        intent.putExtra("sessionID",map.get("sessionID").toString());
+                        intent.putExtra("sessionToken",map.get("sessionToken").toString());
+                        intent.putExtra("currentSessionTableID",currentSessionTableID);
+
                         startActivity(intent);
 
                     }
                 }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                // Loading finished
-                dialog.dismiss();
+//                // Loading finished
+//                dialog.dismiss();
 
                 // Display Some error message on screen.
                 AlertDialog.showSingleButtonAlertDialog(StartLiveTalkActivity.this,"Ok",
                         "Error","Some error occur. Please try again later",
                         new AlertDialogSingleInterface(){
-                    @Override
-                    public void doTaskOnClick() {
-                    }
-                });
+                            @Override
+                            public void doTaskOnClick() {
+                            }
+                        });
             }
         });
     }
-    private void updateSessionSubsciberID(final Map<String,Object> map, String sessionID){
+    private void updateSessionSubsciberID(final Map<String,Object> map, final String sessionID){
 
         // Loading Start
         final Dialog dialog = AlertDialog.showLoadingDialog(StartLiveTalkActivity.this);
         dialog.show();
 
         db.collection("Sessions")
-            .document(sessionID)
-            .update(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                .document(sessionID)
+                .update(map).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 // Loading finished
                 dialog.dismiss();
 
-                // Go to StartLiveTalkActivity with sessionKey
+                currentSessionTableID = sessionID;
+
+                // Go to StartLiveTalkActivity with sessionID
                 Intent intent = new Intent(StartLiveTalkActivity.this,VideoChatActivity.class);
-                intent.putExtra("sessionKey",map.get("sessionKey").toString());
+                intent.putExtra("sessionID",map.get("sessionID").toString());
+                intent.putExtra("sessionToken",map.get("sessionToken").toString());
+                intent.putExtra("currentSessionTableID",currentSessionTableID);
+
+
                 startActivity(intent);
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -260,7 +280,7 @@ public class StartLiveTalkActivity extends AppCompatActivity {
     }
     private void deleteSession(){
         db.collection("Sessions")
-                .document(currentSessionID)
+                .document(currentSessionTableID)
                 .delete().addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
